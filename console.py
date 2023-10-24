f4650041cf6d4fa44ec60c74c044983b5bf3ef65
gastonperego <gastonperego@gmail.com>
2023-10-24 14:34:36 +0000

    fix: empty line

--- console.py
+++ console.py
@@ -4,17 +4,23 @@ import cmd
 
 
 if __name__ == '__main__':
-    
+
     class HBNBCommand(cmd.Cmd):
         """console"""
 
         prompt = "(hbnb) "
         intro = ""
 
+        def emptyline(self):
+            """Pass if the input is an empty line"""
+
+        def do_quit(self, args):
+            """Quit command to exit the program"""
+            exit()
+
         def do_EOF(self, args):
             """Exits from the console"""
             print()
             exit()
 
-HBNBCommand().cmdloop()
-
+    HBNBCommand().cmdloop()
