1f42e640ffdc65e462b4758ad087808903da6c24
root <gastonperego@gmail.com>
2023-10-27 17:26:14 +0000

    method all added, method destroy almost fixed

--- console.py
+++ console.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
             print()
             exit()
             
-        def do_create(self, arg):
+        def do_create(self, arg):  
             """Creates a BaseModel instance"""
             args = arg.split()
             if len(args) == 0:
@@ -50,15 +50,15 @@ if __name__ == '__main__':
                 with open("file.json", "r", encoding="utf-8") as file:
                     dic = json.loads(file.read())
                     for key in dic.keys():
-                        id = key.split(".")
-                        if id[1] == args[1]:
+                        cls_id = key.split(".")
+                        if cls_id[1] == args[1]:
                             instance = models.base_model.BaseModel(dic[key])
                             print(instance)
                             flag = 1
                     if flag != 1:
                         print("** no instance found **")
         
-        def do_destroy(self, arg):
+        def do_destroy(self, arg):                  # Falta agregar que borre la instancia del "__objects" porque si no cada vez q borramos una instancia y creamos una nuev la que borramos se vuelve a crear
             "Erases an instance from the json file based on the class name and id."
             args = arg.split()
             if len(args) == 0:
@@ -72,14 +72,34 @@ if __name__ == '__main__':
                 with open("file.json", "r+", encoding="utf-8") as file:
                     dic = json.loads(file.read())
                     for key in dic.keys():
-                        id = key.split(".")
-                        if id[1] == args[1]:
+                        cls_id = key.split(".")
+                        if cls_id[1] == args[1]:
                             key_copy = key
                             flag = 1
                     dic.pop(key_copy)
                 with open("file.json", "w", encoding="utf-8") as file:
-                    file.write(json.dumps(str(dic)))    
+                    json.dump(dic, file)
                 if flag != 1:
                     print("** no instance found **")
+
+        def do_all(self, arg):
+            """Prints all the instances of a class"""
+            lis = []
+            if arg == "BaseModel":
+                with open("file.json", "r", encoding="utf-8") as file:
+                    dic = json.load(file)
+                    for key, value in dic.items():
+                        cls_id = key.split(".")
+                        if cls_id[0] == "BaseModel":
+                            instance = models.base_model.BaseModel(value)
+                            lis.append(instance.__str__())
+                print(lis)
+            else:
+                with open("file.json", "r", encoding="utf-8") as file:
+                    dic = json.load(file)
+                    for key, value in dic.items():
+                            instance = models.base_model.BaseModel(value)
+                            lis.append(instance.__str__())
+                print(lis)
 
     HBNBCommand().cmdloop()
