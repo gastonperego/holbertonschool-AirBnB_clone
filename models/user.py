e6c9b58a939174714fd595706cebba61785c182c
jorge871 <jg303114@gmail.com>
2023-10-28 13:49:42 -0300

    fix: style error

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""class user"""
+"""user model"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """class user"""
     email = ''
     password = ''
     first_name = ''
