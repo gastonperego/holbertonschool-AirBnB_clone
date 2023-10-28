ebd2072e743f6c1ed69d8a3bfd6fe20340b3d014
jorge871 <jg303114@gmail.com>
2023-10-28 13:41:35 -0300

    Fix: style error

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ import cmd
 import models
 import json
 
+
 class HBNBCommand(cmd.Cmd):
     """console"""
 
@@ -21,8 +22,8 @@ class HBNBCommand(cmd.Cmd):
         """Exits from the console"""
         print()
         exit()
-        
-    def do_create(self, arg):  
+
+    def do_create(self, arg):
         """Creates a BaseModel instance"""
         args = arg.split()
         if len(args) == 0:
@@ -33,9 +34,10 @@ class HBNBCommand(cmd.Cmd):
             instance = models.base_model.BaseModel()
             instance.save()
             print(f"{instance.id}")
-            
+
     def do_show(self, arg):
-        "Prints the string representation of an instance based on the class name and id."
+        """Prints the string representation of
+        an instance based on the class name and id."""
         args = arg.split()
         if len(args) == 0:
             print("** class name missing **")
@@ -55,10 +57,12 @@ class HBNBCommand(cmd.Cmd):
                         key = 1
                 if key != 1:
                     print("** no instance found **")
-            
-    
-    def do_destroy(self, arg):                  # Falta agregar que borre la instancia del "__objects" porque si no cada vez q borramos una instancia y creamos una nuev la que borramos se vuelve a crear
+
+
+    def do_destroy(self, arg):
+        # Falta agregar que borre la instancia del "__objects" porque si no cada vez q borramos una instancia y creamos una nuev la que borramos se vuelve a crear
         "Erases an instance from the json file based on the class name and id."
+
         args = arg.split()
         if len(args) == 0:
             print("** class name missing **")
@@ -102,7 +106,7 @@ class HBNBCommand(cmd.Cmd):
         args = arg.split()
         if len(args) == 0:
             print("** class name missing **")
-         
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
